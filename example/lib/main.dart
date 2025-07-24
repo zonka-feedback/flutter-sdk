8742b325214fde93eef4045ea0d40805b7a88c2c
Manav Garg <manavgarg@CIadmins-MacBook-Air-2.local>
2025-07-24 14:49:15 +0530

    push code

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       home: ZonkaFeedBackSurvey(),
     );
   }
